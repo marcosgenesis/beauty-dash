37466798ba9320aba49382cf3577ae2f6bcb0791
marcosgenesis <marcosgenesisof@gmail.com>
2021-01-23 23:28:19 -0300

    ✨ first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import styles from "../styles/Home.module.css";
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+  CartesianGrid,
+} from "recharts";
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <div className={styles.emissionArea}>
+        <div className={styles.emissionChart}>
+          <span className={styles.emissionHeader}>
+            <h2>Emissões</h2>
+            <p>Número de emissões feitas </p>
+          </span>
+          <LineChart
+            width={550}
+            height={278}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <CartesianGrid strokeDasharray="3 3" stroke="#e2e2e2" />
+            <XAxis dataKey="name" stroke="#E2E2E2" />
+            <YAxis stroke="#E2E2E2" />
+            <Tooltip />
+            <Legend
+              verticalAlign="top"
+              align="right"
+              wrapperStyle={{ top: -50 }}
+            />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#5B6EB2"
+              strokeWidth={2}
+            />
+            <Line type="monotone" dataKey="uv" stroke="#C0C5CE" />
+          </LineChart>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={styles.emissionColumn}>
+          <div className={styles.seeMoreEmission}></div>
+          <div className={styles.emissionCompletedContainer}></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
